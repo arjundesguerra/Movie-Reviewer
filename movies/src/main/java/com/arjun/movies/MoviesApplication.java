3cb1b0a73d6bcf5f3e029cd5ae52dae1e8ed60a1
Arjun Esguerra <arjundesguerra@gmail.com>
2023-11-15 04:15:12 -0800

    added spring files

--- movies/src/main/java/com/arjun/movies/MoviesApplication.java
+++ movies/src/main/java/com/arjun/movies/MoviesApplication.java
@@ -1,4 +1,4 @@
-package dev.arjun.movies;
+package com.arjun.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- movies/src/test/java/com/arjun/movies/MoviesApplicationTests.java
+++ movies/src/test/java/com/arjun/movies/MoviesApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.arjun.movies;
+package com.arjun.movies;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
