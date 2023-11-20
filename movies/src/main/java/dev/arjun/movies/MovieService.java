13d6b10961955fc9add148e43298cc0052f95a91
Arjun Esguerra <arjundesguerra@gmail.com>
2023-11-20 02:36:45 -0800

    created getSingleMovie function

--- movies/src/main/java/dev/arjun/movies/MovieService.java
+++ movies/src/main/java/dev/arjun/movies/MovieService.java
@@ -1,17 +1,23 @@
 package dev.arjun.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
-
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    // Optional because passed id may return a null.
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
     }
 
 }
