1895d5149b237b4d914543209d3e9cb0fa91be3f
Arjun Esguerra <arjundesguerra@gmail.com>
2023-11-21 03:36:36 -0800

    wrote comments for the review service class

--- movies/src/main/java/dev/arjun/movies/ReviewService.java
+++ movies/src/main/java/dev/arjun/movies/ReviewService.java
@@ -1,6 +1,5 @@
 package dev.arjun.movies;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -15,12 +14,14 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId) {
         Review review = reviewRepository.insert(new Review(reviewBody));
-        // creates review object with a passed in body and adds review to the review repository
+        // This line creates a new Review object with the specified reviewBody and inserts it into the ReviewRepository.
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
+        // This block of code updates a Movie object in the MongoDB database associated with the provided imdbId.
+        // It uses the MongoTemplate to apply an update operation, specifically pushing the new review's ID into the "reviewIds" field of the Movie.
 
         return review;
     }
